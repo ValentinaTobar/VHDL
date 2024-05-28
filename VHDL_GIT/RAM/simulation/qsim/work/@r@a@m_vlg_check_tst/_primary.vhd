21b9c3d71c43493740a804329d178337356a1419
ValentinaTobar <valentinatobar@unicauca.edu.co>
2024-05-28 16:57:00 -0500

    VHDL_Memorytest

--- VHDL_GIT/RAM/simulation/qsim/work/@r@a@m_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity RAM_vlg_check_tst is
-    port(
-        data_out        : in     vl_logic_vector(7 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end RAM_vlg_check_tst;
